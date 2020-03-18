6e444f7181d53c2cfcb4f96768dc1ee0c47a1797
madhuvandhi <Madhuvandhi-97>
2020-03-18 13:30:06 +0530

    add all files

---
+++ hello.py
@@ -0,0 +1,35 @@
+print("helloooo")
+
+
+
+if 5>4:
+    print("ghahhahah")
+else:   
+    print("ahahahahhahah")
+
+import random
+print(random.randrange(0,50))
+
+def myfunc():
+    
+    x="prrttyy"
+    print(x)
+myfunc()
+print("hrllo"+"world")
+
+age=23
+weight=51
+height=157
+text="im madhu and my age is {0} and weight is{2}, height is{1}"
+print(text.format(age,height,weight))
+
+print("my name is \"madhu\"")
+y="masdhu"
+print(y.capitalize())
+
+z="helllo"
+print(z.count("l"))
+
+h="My name is M♪dhu"
+u=h.encode()
+print(u)
